3b6746157339bd468b3235471b44f1de839b5482
Dani <gabood012@gmail.com>
2026-07-11 13:44:53 +0800

    Add files via upload

---
+++ PrintCharacter.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class PrintCharacter {
+    public static void main(String[] args) {
+         Scanner input = new Scanner(System.in);
+        int numberPerLine = input.nextInt();
+        printChars('1', 'Z', numberPerLine);
+        System.out.println(" Done!");
+    }
+public static void printChars(char c1, char c2, int n)
+    {
+        int count = 0;
+        while(true)
+        {
+            System.out.print(c1 + " ");
+            count++;
+        
+            if (count % n == 0) {
+                System.out.println();
+            }
+        
+            if (c1 == c2) {
+                break;  
+            }
+            c1++;  
+        }
+    }
+
+  
+}
+
+
